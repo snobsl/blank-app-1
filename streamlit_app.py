68b0379d35923a6590a1dbbaf9615981ebed1dc5
snobsl <snobslam@gmail.com>
2024-11-20 19:22:02 +0000

    update (tampilan awal)

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,35 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+#App With Streamlit
+# Judul aplikasi
+st.title("📊 Aplikasi Sentimen Analisis")
+
+# Deskripsi aplikasi
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Selamat datang di aplikasi sentimen analisis!"
 )
+st.write(
+    "Karya Leonard Agust Laga Lajar"
+)
+st.write(
+    "Untuk melakukan sentimen analisis, silahkan masukkan teks."
+)
+
+# Input teks dari pengguna
+user_input = st.text_area("Masukkan teks untuk analisis sentimen:")
+
+# Membersihkan teks
+clean_input = clean_text(user_input)
+
+# Menampilkan hasil prediksi
+# Naive Bayes
+st.subheader("1. Naive Bayes")
+st.write(f"Prediksi Naive Bayes: belum ada data")
+
+# KNN
+st.subheader("2. KNN")
+st.write(f"KNN Prediction: belum ada data")
+
+# SVM
+st.subheader("3. SVM")
+st.write(f"SVM Prediction: belum ada data")
